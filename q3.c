756a3eaed42a263f899d4912bbced7b87afdb71a
user <matanlevy90@gmail.com>
2017-05-10 20:15:43 +0300

    q1

---
+++ q3.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main() {
+#error "Remove this line and add your code here!""
+    return 0;
+}
---
+++ q4.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/user.h>
+
+
+int main(int argc, char **argv) {
+    // Make the malware stop waiting for our output by forking a child process:
+    if (fork() != 0) {
+        // Kill the parent process so we stop waiting from the malware
+        return 0;
+    } else {
+        // Close the output stream so we stop waiting from the malware
+        fclose(stdout);
+    }
+
+    // The rest of your code goes here
+#error "Remove this line and add your code here!""
+    return 0;
+}
